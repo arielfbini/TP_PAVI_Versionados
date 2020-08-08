a6dbaffd9b6331a313db416771ab4a01109fd22d
arielfbini <arielfbini@gmail.com>
2020-08-08 18:11:42 -0300

    Agregado los nombre de los labels

--- TP_Login_Versiones/Form1.Designer.cs
+++ TP_Login_Versiones/Form1.Designer.cs
@@ -31,8 +31,8 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txt_contraseña = new System.Windows.Forms.TextBox();
             this.txt_usuario = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_contraseña = new System.Windows.Forms.Label();
+            this.lbl_nombre = new System.Windows.Forms.Label();
             this.btn_aceptar = new System.Windows.Forms.Button();
             this.btn_cancelar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -42,8 +42,8 @@
             // 
             this.groupBox1.Controls.Add(this.txt_contraseña);
             this.groupBox1.Controls.Add(this.txt_usuario);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lbl_contraseña);
+            this.groupBox1.Controls.Add(this.lbl_nombre);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(264, 113);
@@ -64,23 +64,23 @@
             this.txt_usuario.Size = new System.Drawing.Size(115, 20);
             this.txt_usuario.TabIndex = 2;
             // 
-            // label2
+            // lbl_contraseña
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 63);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Contraseña";
+            this.lbl_contraseña.AutoSize = true;
+            this.lbl_contraseña.Location = new System.Drawing.Point(31, 63);
+            this.lbl_contraseña.Name = "lbl_contraseña";
+            this.lbl_contraseña.Size = new System.Drawing.Size(61, 13);
+            this.lbl_contraseña.TabIndex = 1;
+            this.lbl_contraseña.Text = "Contraseña";
             // 
-            // label1
+            // lbl_nombre
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre";
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Location = new System.Drawing.Point(31, 28);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(44, 13);
+            this.lbl_nombre.TabIndex = 0;
+            this.lbl_nombre.Text = "Nombre";
             // 
             // btn_aceptar
             // 
@@ -122,8 +122,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txt_contraseña;
         private System.Windows.Forms.TextBox txt_usuario;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_contraseña;
+        private System.Windows.Forms.Label lbl_nombre;
         private System.Windows.Forms.Button btn_aceptar;
         private System.Windows.Forms.Button btn_cancelar;
     }
